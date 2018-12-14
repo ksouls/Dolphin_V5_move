76686ddaacbf32a4eaa6d8deae24489a63d84755
ksouls <38291123+ksouls@users.noreply.github.com>
2018-12-14 14:20:28 +0000

    Add files via upload

---
+++ Dolphin_V5/Dolphin_V5/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dolphin_V5
+//
+//  Created by an16aal on 14/12/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var Dolphin: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
